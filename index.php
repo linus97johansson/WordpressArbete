9b085a8a7ae0173d7d33ac28c2922dc63ec2e561
linus97johansson <linus97johansson@gmail.com>
2018-05-16 08:51:53 +0200

    whha

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php
-echo 'Speech is silver. ';
-get_header();
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>My static title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-get_footer();
+<body>
+<? get_head(); ?>
+<main>
+    <h1>The page title</h1>
+    <p>The content</p>
+</main>
+<? get_footer(); ?>
+</body>
 
-?>
+</html>
