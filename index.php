12930c9b0f556b803473a1725d7fbd2194e411f0
linus97johansson <linus97johansson@gmail.com>
2018-06-11 09:25:47 +0200

    Revert "srsrngj"
    This reverts commit 9bd5ec4dc0e41600124adde1a0f6d06a51585f9f.

--- index.php
+++ index.php
@@ -6,7 +6,25 @@ $args = array(
 $my_query = new WP_Query($args);
 ?>
 <main>
-<h1>Something seems to be wrong here</h1>
+    <h1>The page title - index</h1>
+    <p>
+        <?
+        if ($my_query->have_posts()) {
+
+            while ($my_query->have_posts()) {
+
+                $my_query->the_post();
+                echo the_title();
+                the_content();
+            }
+        }else {
+            echo"no found- index";
+
+        }
+
+        ?>
+    </p>
+
 </main>
 <?php
     get_footer();
