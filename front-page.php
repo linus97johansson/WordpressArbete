0889c2477410a2334649e3f06224906f617eaf37
Axel Tidehorn <axel.tidehorn.96@gmail.com>
2018-06-11 10:07:54 +0200

    dör

--- front-page.php
+++ front-page.php
@@ -14,28 +14,36 @@ $my_query = new WP_Query( $args);
 
 <!-- Vet att man inte ska ladda in jquery här, men fick det att fungera iaf.. -->
 <script src="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.min.js"></script>
-<main>
+<div id="slider">
+    <?php
 
-    <div id="slider">
-        <?php
+    if ($my_query->have_posts()) {
+        while ($my_query->have_posts()) {
 
-        if ($my_query->have_posts()) {
-            while ($my_query->have_posts()) {
-
-                $my_query->the_post();
-                print"<div>";
-                the_post_thumbnail(none,array('class'=>'sliderImage'));
-                print"<div>".the_title()."</div>";
-                print"</div>";
-            }
-
+            $my_query->the_post();
+            print"<div>";
+            the_post_thumbnail(none,array('class'=>'sliderImage'));
+            print"<div class='featuredtext'><p>";
+            the_title();
+            print"</p></div>";
+            print"</div>";
         }
 
-        ?>
-    </div>
+    }
+
+    ?>
+</div>
+<main>
+
+
   <div class="information-container">
+    <div class="flex">
+
+
     <?php dynamic_sidebar( 'tider_widget' ); ?>
     <?php dynamic_sidebar('lunch_widget') ?>
+
+    </div>
   </div>
 </main>
 <?php get_footer(); ?>
---
+++ template-meny.php
@@ -0,0 +1,115 @@
+<?php
+    /*  Template Name: template-Meny
+   * Desription: Det här är menyn
+   */
+
+get_header();
+
+$terms_categories = get_terms( array(
+        'taxonomy' => 'Category',
+        'hide_empty' => true,
+    )
+);
+$terms_prices = get_terms( array(
+        'taxonomy' => 'Price',
+        'hide_empty' => true,
+    )
+);
+$categories= array();
+foreach ($terms_categories as $category){
+    //var_dump($category->name);
+        if($category->name != "Uncategorized") {
+            array_push($categories, $category->name);
+        }
+    };
+
+$prices = array();
+foreach ($terms_prices as $price){
+    //var_dump($price->name);
+    array_push($prices, $price->name);
+};
+
+
+?>
+<main>
+    <h1>The page title</h1>
+    <p>
+
+        <?
+
+        foreach ($categories as $dishType){
+
+
+            print "<div class='".$dishType."'><h2 id='dish'>".$dishType."</h2>";
+
+            if($dishType == "Pizza"){
+                print"<ol class='meny-container'>";
+            }else{
+                print"<ul class='meny-container-ul'>";
+            }
+
+            foreach ($prices as $dishPrice){
+
+                $args = array(
+                    "post_type" => "dishes",
+                    "posts_per_page" => -1,
+                    'tax_query' => array(
+                        'relation' => 'AND',
+                        array(
+                            'taxonomy' => 'Price',
+                            'field' => 'name',
+                            'terms'    => $dishPrice,
+                        ),
+                        array(
+                            'taxonomy' => 'Category',
+                            'field' => 'name',
+                            'terms'    => $dishType,
+                        ),
+                    ),
+
+
+                );
+
+                $my_query = new WP_Query( $args);
+
+                if ($my_query->have_posts()) {
+                    while ($my_query->have_posts()) {
+
+                        $my_query->the_post();
+                        if($dishType=="Pizza") {
+                            print"<li class='pizza'><h3>";
+                        }else{
+                            print"<li><h3>";
+                        }
+                        the_title();
+                        print " ".$dishPrice . "kr";
+                        print"</h3><p>";
+                        the_content();
+                        print"</p>
+                        </li>";
+
+                    }
+
+                }
+                unset($args);
+                wp_reset_query();
+
+
+
+            }
+            if($dishType == "Pizza"){
+                print"</ol>";
+            }else{
+                print"</ul>";
+            }
+            print"</div>";
+
+        }
+        print "</ol>";
+
+        ?>
+    </p>
+</main>
+<?php
+get_footer();
+?>
