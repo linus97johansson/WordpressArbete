df37656e198df6ce3139fcb409c780d010407b12
linus97johansson <linus97johansson@gmail.com>
2018-06-11 09:26:14 +0200

    Revert "Revert "srsrngj""
    This reverts commit 12930c9b0f556b803473a1725d7fbd2194e411f0.

--- template-meny.php
+++ template-meny.php
@@ -1,30 +1,8 @@
 <?php
-    /*  Template Name: Meny
+    /*  Template Name: template-Meny
    * Desription: Det här är menyn
    */
-/*
- * $args = array(
-    "post_type" => "dishes",
-    "posts_per_page" => -1,
-    'tax_query' => array(
-        'relation' => 'AND',
-        array(
-            'taxonomy' => 'Price',
-            'field' => 'name',
-            'terms'    => '85',
-        ),
-            array(
-                'taxonomy' => 'Category',
-                'field' => 'name',
-                'terms'    => 'Pizza',
-            ),
-    ),
 
-
-
-);
-$my_query = new WP_Query( $args);
- */
 get_header();
 
 $terms_categories = get_terms( array(
@@ -50,64 +28,6 @@ foreach ($terms_prices as $price){
     //var_dump($price->name);
     array_push($prices, $price->name);
 };
-
-/*
-foreach ($categories as $dishType){
-
-    print "<h2 id='dish'>".$dishType."</h2>";
-
-    foreach ($prices as $dishPrice){
-
-        $args = array(
-            "post_type" => "dish",
-            "posts_per_page" => -1,
-            'tax_query' => array(
-                'relation' => 'AND',
-                array(
-                    'taxonomy' => 'Price',
-                    'field' => 'name',
-                    'terms'    => $dishPrice,
-                ),
-                array(
-                    'taxonomy' => 'Category',
-                    'field' => 'name',
-                    'terms'    => $dishType,
-                ),
-            ),
-
-
-
-        );
-        $my_query = new WP_Query( $args);
-
-        if ($my_query != null) {
-            print"<ol style='margin-left:50px;'>";
-            while ($my_query->have_posts()) {
-
-                $my_query->the_post();
-                print"<li>";
-                the_title();
-                print"</li><p>";
-                the_content();
-                print"</p>";
-            }
-
-            print"</ol>";
-        }else {
-            echo"no found- meny";
-
-        }
-
-        unset($args);
-        wp_reset_postdata();
-
-
-    }
-
-}
-*/
-
-
 
 
 ?>
@@ -156,7 +76,11 @@ foreach ($categories as $dishType){
                     while ($my_query->have_posts()) {
 
                         $my_query->the_post();
-                        print"<li><h3>";
+                        if($dishType=="Pizza") {
+                            print"<li class='pizza'><h3>";
+                        }else{
+                            print"<li><h3>";
+                        }
                         the_title();
                         print " ".$dishPrice . "kr";
                         print"</h3><p>";
